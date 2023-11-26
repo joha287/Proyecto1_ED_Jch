ead9c39a09ab17aca4e28cdb54cf668ce59939c7
joha287 <150701969+joha287@users.noreply.github.com>
2023-11-26 20:12:33 +0100

    Update Clase1ED.java

--- src/es/iescanaveral/daw1/ed/Jch1/Clase1ED.java
+++ src/es/iescanaveral/daw1/ed/Jch1/Clase1ED.java
@@ -4,7 +4,7 @@ import es.iescanaveral.daw1.ed.Jch1.Clase2ED;
 
 public class Clase1ED {
 	public static void main(String[] args) {
-		
+//esto se añadio desde la web de github
 		Clase2ED clase2 = new Clase2ED();
 		clase2.saludar("Jch");
 
